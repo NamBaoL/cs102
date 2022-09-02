278b8500dfa2738756c388a8b44c242f924e1da4
Bao Nam <86450453+NamBaoL@users.noreply.github.com>
2022-09-02 08:39:34 +0700

    Delete src directory

--- src/entities/bullet.py
+++
@@ -1,7 +0,0 @@
-from entities.movable_entity import MovableEntity
-
-
-class Bullet(MovableEntity):
-    def __init__(self, damage, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.damage = damage
--- src/entities/trampoline.py
+++
@@ -1,8 +0,0 @@
-from entities.animated_entity import PropEntity
-
-
-class Trampoline(PropEntity):
-    """
-    Trampoline item for the TRAMPOLINE_QUEST. This class just inherits from the parent class and
-    does not need any custom behavior hence the body is empty.
-    """
